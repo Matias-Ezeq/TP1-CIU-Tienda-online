60034f20154c2853cd91b6e1f9675e030f7f3f21
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-05-22 18:48:20 -0300

    Página de error 404 básica creada

--- tienda-online/src/App.jsx
+++ tienda-online/src/App.jsx
@@ -1,12 +1,15 @@
+import { Route, Routes } from "react-router"
+import Error404 from "./components/Error404.jsx"
 import Header from "./components/Header.jsx"
 
 function App() {
   return (
     <>
     <Header/>
-    <routes>
-      <route path='/' element={<></>}/>
-    </routes>
+    <Routes>
+      <Route path='/' element={<></>}/>
+      <Route path='*' element={<Error404/>}/>
+    </Routes>
     </>
   )
 }
---
+++ tienda-online/src/components/Error404.jsx
@@ -0,0 +1,10 @@
+function Error404() {
+    return(
+        <div>
+            <h1>Oops!</h1>
+            <h3>Página no encontrada</h3>
+        </div>
+    )
+}
+
+export default Error404
