8321f4f449de381aca045e213d2dc0d6aac35798
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-05-22 16:26:03 -0300

    embellecido parcial del header implementando react-bootstrap

--- tienda-online/src/components/Header.jsx
+++ tienda-online/src/components/Header.jsx
@@ -1,11 +1,42 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Header(){
-    return(
-        <header style={{display:"flex", justifyContent:"space-between"}}>
-            <div>placeholder logo</div> <div><input name="busqueda" type="search" placeholder="Buscar en el catálogo"/><input name="buscar" type="button" value="Buscar"/></div> <button><FontAwesomeIcon icon="fa-solid fa-cart-shopping" /></button>
-        </header>
-    )
+  return(
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#inicio">GameGarage</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#inicio">Inicio</Nav.Link>
+            <Nav.Link href="#novedades">Novedades</Nav.Link>
+          </Nav>
+          <Form inline>
+        <Row>
+          <Col xs="auto">
+            <Form.Control
+              type="text"
+              placeholder="Buscar en el catálogo"
+              className=" mr-sm-2"
+            />
+            </Col>
+        <Col xs="auto">
+            <Button type="submit">Buscar</Button>
+          </Col>
+        </Row>
+      </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
 
 export default Header
