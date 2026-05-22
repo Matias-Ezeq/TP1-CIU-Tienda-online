597dc3b301a7920541ff99debd3a0f414e9aacad
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-05-22 18:48:54 -0300

    Embellecimiento del Header e implementación de links via react-router Link

--- tienda-online/src/components/Header.jsx
+++ tienda-online/src/components/Header.jsx
@@ -1,41 +1,53 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Link } from "react-router"
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 
 function Header(){
   return(
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#inicio">GameGarage</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#inicio">Inicio</Nav.Link>
-            <Nav.Link href="#novedades">Novedades</Nav.Link>
-          </Nav>
-          <Form inline>
-        <Row>
-          <Col xs="auto">
-            <Form.Control
-              type="text"
-              placeholder="Buscar en el catálogo"
-              className=" mr-sm-2"
-            />
+    <header>
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="d-flex flex-column align-items-center" >
+      <Container fluid>
+        <Navbar.Brand as={Link} to='/' className="me-auto">
+          <FontAwesomeIcon icon="fa-solid fa-gamepad" />GameGarage
+        </Navbar.Brand>
+        <Form inline>
+          <Row>
+            <Col xs="auto">
+              <InputGroup>
+                <Form.Control
+                  type="text"
+                  placeholder="Buscar en el catálogo"
+                  className=" mr-sm-2"
+                />
+                <Button variant="secondary" id="Buscar">
+                  <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                </Button>
+              </InputGroup>
             </Col>
-        <Col xs="auto">
-            <Button type="submit">Buscar</Button>
-          </Col>
-        </Row>
-      </Form>
-        </Navbar.Collapse>
+          </Row>
+        </Form>
+        <Button href="#carrito" variant="secondary" className="ms-auto">
+          <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+        </Button>
+      </Container>
+      <Container fluid>
+        <Nav variant="underline" defaultActiveKey="#inicio">
+          <Nav.Link as={Link} to='/'>Inicio</Nav.Link>
+          <Nav.Link as={Link} to="/catalogo">Catálogo</Nav.Link>
+          <Nav.Link as={Link} to="/novedades">Novedades</Nav.Link>
+          <Nav.Link as={Link} to="/ofertas">Ofertas</Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
+    </header>
   )
 }
 
