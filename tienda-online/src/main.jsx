6e7a2791b133a2873a18ace00bca2b70bc41c26a
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-05-22 15:49:58 -0300

    Importación del paquete de íconos FontAwesome

--- tienda-online/src/main.jsx
+++ tienda-online/src/main.jsx
@@ -4,6 +4,16 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
